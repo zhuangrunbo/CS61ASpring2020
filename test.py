8fd797de75157ccc4dd9fc45d12b7bac7d274975
runbozhuang <1206904210@qq.com>
2020-02-07 13:59:32 +0800

    simple print test case

--- test.py
+++ test.py
@@ -1,14 +1,5 @@
-from operator import floordiv, mod
-def divide_exact(n, d):
-    """ doctest
-    >>> divide_exact(2013, 10)
-    >>> q
-    201
-    >>> r
-    3
-    """
-    return floordiv(n, d), mod(n, d)
+def test():
+    str = "hello"
+    print(str)
 
-"""q, r = divide_exact(2013, 10)"""
-python -m doctest -v
-
+test()
