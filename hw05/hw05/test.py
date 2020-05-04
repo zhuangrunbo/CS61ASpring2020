132df9d238a662c69ddb995acd251951fc7eb654
Runbo Zhuang <runbo.zhuang@configura.com>
2020-05-04 19:34:46 +0800

    hw05, all questions are resovled except option questions, QAQ. ... have a good day

---
+++ hw05/hw05/test.py
@@ -0,0 +1,20 @@
+test = [1, 1, 2]
+test2 = [3, 4]
+test3 = [5, 6]
+#test3.append(1)
+
+
+#test3 += test2
+#print(test3)
+
+print(next(iter(list(set(test)))))
+
+
+#test.append([3])
+#test += test2
+#print(test)
+#print(test+test2)
+
+
+
+
